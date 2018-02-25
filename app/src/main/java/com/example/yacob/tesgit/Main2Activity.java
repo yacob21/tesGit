0397857eae51306f3bb9b48aff54ad63f1b785eb
Yacob <yacob21@yahoo.com>
2018-02-25 22:31:44 +0700

    tambah activity

--- app/src/main/java/com/example/yacob/tesgit/Main2Activity.java
+++ app/src/main/java/com/example/yacob/tesgit/Main2Activity.java
@@ -2,14 +2,12 @@ package com.example.yacob.tesgit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toast.makeText(this, "heheheuheuhuehu", Toast.LENGTH_SHORT).show();
+        setContentView(R.layout.activity_main2);
     }
 }
