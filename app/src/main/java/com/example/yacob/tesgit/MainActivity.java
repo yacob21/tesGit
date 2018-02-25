1f1e042b6269f2ddf1c528e213e2d391335785ee
Yacob <yacob21@yahoo.com>
2018-02-25 22:40:35 +0700

    cabang1

--- app/src/main/java/com/example/yacob/tesgit/MainActivity.java
+++ app/src/main/java/com/example/yacob/tesgit/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "heheheuheuhuehu", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "cabang1", Toast.LENGTH_SHORT).show();
     }
 }
